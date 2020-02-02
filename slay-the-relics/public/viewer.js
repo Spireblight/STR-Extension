c1d8263d8d4d57796987f9f9fdc40cfd8ef657e1
Adam Volný <adam.volny@gmail.com>
2020-02-02 20:17:03 +0100

    create tooltips, add proper resizing on stream resizing.

--- slay-the-relics/public/viewer.js
+++ slay-the-relics/public/viewer.js
@@ -1,10 +1,11 @@
-// because who wants to type this every time?
-var twitch = window.Twitch.ext;
 
+var twitch = window.Twitch.ext;
+var last_relics = []
+var current_tooltip_id = -1
 
 const MSG_TYPE_SET_RELICS = "set_relics"
 
-// create the request options for our Twitch API calls
+
 
 twitch.onContext(function(context) {
     twitch.rig.log(context);
@@ -29,14 +30,17 @@ function receiveMessage(msg) {
     }
 }
 
-
-const ITEM_WIDTH = 3.75
-const ITEM_LEFT = 1.458//3.3333
-const ITEM_OFFSET_LARGE = 1.875
-const MAX_DISPLAY_ITEMS = 25
-
+const ITEM_WIDTH = 3.75 //%
+const ITEM_LEFT = 1.458 //%
+const ITEM_OFFSET_LARGE = 1.875 //%
+const MAX_DISPLAY_ITEMS = 25 //count
+const MAX_RIGHT = 97.083 //%
+const TOOLTIP_WIDTH = 315 //px
 
 function setRelics(relics) {
+    current_tooltip_id = Math.min(relics.length, current_tooltip_id)
+    last_relics = relics
+
     var items = document.getElementById('items')
     
     var html = ''
@@ -44,19 +48,65 @@ function setRelics(relics) {
     for (let i = 0; i < relics.length && i < MAX_DISPLAY_ITEMS; i++) {
         const item = relics[i];
         
-        var x = ITEM_LEFT + i * ITEM_WIDTH + (relics.length > 25 ? 1 : 0) * ITEM_OFFSET_LARGE
+        var x_placeholder = ITEM_LEFT + i * ITEM_WIDTH + (relics.length > 25 ? 1 : 0) * ITEM_OFFSET_LARGE
+        var stream_width = $('#items').width()
+        var max_x_tooltip = ((stream_width * MAX_RIGHT) - TOOLTIP_WIDTH) / stream_width
+        var x_tooltip = Math.min(x_placeholder, max_x_tooltip)
 
-        html = html.concat(createItemHTML(x, item.name, item.description))
+        console.log(i, 'stream width', stream_width, 'x_placeholder', x_placeholder, 'max_x_tooltip', max_x_tooltip)
+
+        html = html.concat(createItemHTML(x_placeholder, x_tooltip, item.name, item.description, i))
     }
 
     items.innerHTML = html
 
-    log('setting inner html to: ' + html)
+    console.log('setting inner html to: ' + html)
 
-    function createItemHTML(x, name, description) {
-        var html = '<div class="item_placeholder" style="left: ' + x + '%" title="' + name + ': ' + description + '"></div>'
+    function createItemHTML(x_placeholder, x_tooltip, name, description, id) {
+        var html = '<div class="item_placeholder" style="left: ' + x_placeholder + '%" ' // title="' + name + ': ' + description + '"
+        html = html.concat('onmouseover="showTooltip(\'tooltip_' + id + '\')" ')
+        html = html.concat('onmouseout="hideTooltip(\'tooltip_' + id + '\')"></div>')
+        html = html.concat('<div class="tooltip" id="tooltip_' + id + '" style="left: ' + x_tooltip + '%">')
+        html = html.concat('<div class="tooltip_header">' + name + '</div>')
+        html = html.concat('<div>' + replaceColorCodes(description) + '</div>')
+        html = html.concat('</div>');
         return html
+
+        function replaceColorCodes(text) {
+            var parts = text.split(' ')
+
+            for (let i = 0; i < parts.length; i++) {
+                const part = parts[i];
+                
+                if (part.charAt(0) == '#') {
+                    var text_class = ''
+
+                    switch(part.charAt(1)) {
+                        case 'y': text_class = 'text_yellow'; break;
+                        case 'b': text_class = 'text_blue'; break;
+                        case 'r': text_class = 'text_red'; break;
+                    }
+    
+                    var text = part.substring(2)
+                    parts[i] = '<span class="' + text_class + '">' + text + '</span>'
+                }
+            }
+
+            return parts.join(' ')
+        }
     }
+}
+
+
+function showTooltip(id) {
+    current_tooltip_id = id
+    document.getElementById(id).style.display = 'block';
+}
+
+
+function hideTooltip(id) {
+    current_tooltip_id = undefined
+    document.getElementById(id).style.display = 'none';
 }
 
 
@@ -68,11 +118,42 @@ function log(msg) {
 
 $(function() {
 
+    console.log('initialized')
+
+    new ResizeSensor($('#items'), function() {
+        console.log('stream resized')
+        setRelics(last_relics);
+    });
+
     // listen for incoming broadcast message from our EBS
     twitch.listen('broadcast', function (target, contentType, message) {
-        log('received a broadcast message: ' + message)
+        console.log('received a broadcast message: ' + message)
 
         receiveMessage(message);
     });
 
+    $('#items').on('mousemove', function(e) {
+        if (current_tooltip_id) {
+            var left = e.pageX + 46
+            var top = e.pageY + 7 // - 15
+            var stream_width = $('#items').width()
+            var max_left = ((stream_width * MAX_RIGHT) / 100 - TOOLTIP_WIDTH)
+            
+            if (left > max_left) { // display tooltip on the left side of cursor
+                left = e.pageX - 24 - 25 - TOOLTIP_WIDTH
+                top = e.pageY + 7 // + 3
+            }
+
+            console.log('left', left, 'stream_width', stream_width, 'max_left', max_left)
+
+            $('#' + current_tooltip_id).css({
+                left:  left + 'px',
+                top:   top + 'px'
+            });
+        }
+    });
+
+    // TESTING RELICS
+    // var relics = [{"name": "Cracked Core", "description": "At the start of each combat, #yChannel #b1 #yLightning."}, {"name": "Dolly's Mirror", "description": "Upon pickup, obtain an additional copy of a card in your deck."}, {"name": "Smiling Mask", "description": "The Merchant's card removal service now always costs #b50 #yGold."}, {"name": "Orichalcum", "description": "If you end your turn without #yBlock, gain #b6 #yBlock."}, {"name": "Toy Ornithopter", "description": "Whenever you use a potion, heal #b5 HP."}, {"name": "Ink Bottle", "description": "Whenever you play #b10 cards, draw #b1 card."}, {"name": "Omamori", "description": "Negate the next #b2 #rCurses you obtain."}]
+    // setRelics(relics)
 });
