fe8e1a51b55e0df578400474ab8ad6f457a91e55
Adam Volný <adam.volny@gmail.com>
2020-03-28 03:33:03 +0100

    [frontend] fix bug in updates, add custom power tips, placement changes
     * there was a bug in updates, only the power_tip ids were checked for changes but
       not the relevant power tip text
     * custom tips allow for defect orbs display and custom mod text in the future
     * introduced a heuristic for placing the power tips which produces similar results
       to the displaying in game. It's very fiddly and took me a lot of time to come up with
       but it brings satisfying results

--- src/frontend/viewer.js
+++ src/frontend/viewer.js
@@ -3,8 +3,13 @@ var twitch = window.Twitch.ext;
 var last_broadcast = ""
 var last_relics = ""
 var last_potions = ""
+var last_potion_tips = ""
 var last_player_powers = ""
+var last_player_power_tips = ""
 var last_monster_powers_list = ""
+var last_monster_powers_list_tips = ""
+var last_custom_tips_list = ""
+var last_custom_tips_list_tips = ""
 var current_tooltip_id = ""
 var last_broadcast_secs = new Date() / 1000
 
@@ -22,12 +27,12 @@ function receiveMessage(broadcast) {
     if (msg_type == MSG_TYPE_SET_CONTENT) {
         if (last_broadcast !=  broadcast) {
             var character = sanitizeCharacter(msg.character)
+
             setRelics(msg.relics, msg.power_tips, character)
             setPotions(msg.potions, msg.power_tips, character)
-
             setPlayerPowers(msg.player_powers, msg.power_tips, character)
-
             setMonsterPowers(msg.monster_powers, msg.power_tips, character)
+            setCustomTips(msg.custom_tips, msg.power_tips, character)
         }
     } else {
         log('unrecognized msg_type: ' + msg_type)
@@ -42,11 +47,13 @@ const POTION_HITBOX_WIDTH = 2.916 // %
 const MAX_RIGHT = 99.0 //%
 const POWERTIP_WIDTH = 16.406 //%
 const MAX_POWERTIP_MULTICOL_HEIGHT = 70.0 //%
-const CHARACTER_POWERS_OFFSET = 1.041 //%
+const CHARACTER_POWERS_OFFSET_R = 1.0416 //%
+const CHARACTER_POWERS_OFFSET_L = -2.917 //%
+const CHARACTER_HEALTHBAR_HEIGHT = 6.666 //%
 const POWERTIP_BOTTOM_MARGIN = 0.365 //%
-const MULTICOL_COLUMN_RIGHT_MARGIN = 0.469 //%
-const MAX_BOTTOM = 99.0 //%
-const MIN_TOP = 1.0 //%
+const MULTICOL_COLUMN_RIGHT_MARGIN = 0.469 //% - don't mess with this number or the columns won't be separated
+const MAX_BOTTOM = 98.0 //%
+const MIN_TOP = 2.0 //%
 
 const SECS_NOBROADCAST_REMOVE_CONTENT = 70
 
@@ -62,7 +69,7 @@ function sanitizeCharacter(character) {
 
 
 function arraySubset(array, indexes) {
-    subset = []
+    var subset = []
     for (let i = 0; i < indexes.length; i++) {
         const index = indexes[i];
         subset.push(array[index])
@@ -73,24 +80,24 @@ function arraySubset(array, indexes) {
 
 
 function clearItemsByClass(class_to_be_cleared) {
-    console.log('before clear items')
+    // console.log('before clear items')
 
     var items = document.getElementById('items')
     for (let i = items.children.length - 1; i >= 0; i--) {
         const child = items.children[i];
         
         if (child.classList.contains(class_to_be_cleared)) {
-            console.log('clearing child ' + class_to_be_cleared)
+            // console.log('clearing child ' + class_to_be_cleared)
             items.removeChild(child)
         }
     }
 
-    console.log('after clear items')
+    // console.log('after clear items')
 }
 
 
 function setRelics(relics, power_tips, character) {
-    console.log('set relics, relics: ' + JSON.stringify(relics))
+    // console.log('set relics, relics: ' + JSON.stringify(relics))
 
     if (JSON.stringify(relics)  == last_relics) // do not replace 
         return
@@ -105,7 +112,7 @@ function setRelics(relics, power_tips, character) {
 
     var items = document.getElementById('items')
     for (let i = 0; i < relics.items.length; i++) {
-        console.log('adding relic ' + i)
+        // console.log('adding relic ' + i)
 
         const power_tip_indexes = relics.items[i];
         
@@ -128,15 +135,23 @@ function setRelics(relics, power_tips, character) {
 
 
 function setPotions(potions, power_tips, character) {
-    console.log('set potions, potions: ' + JSON.stringify(potions))
+    // console.log('set potions, potions: ' + JSON.stringify(potions))
 
-    if (JSON.stringify(potions) == last_potions)
+    var ids = []
+    for (const power_tips of potions.items) {
+        ids = ids.concat(power_tips)
+    }
+    power_tips_subset = arraySubset(power_tips, ids);
+
+    if (JSON.stringify(potions) == last_potions && JSON.stringify(power_tips_subset) == last_potion_tips) {
         return
+    }
 
     if (current_tooltip_id && current_tooltip_id.startsWith('potion'))
         current_tooltip_id = undefined
 
     last_potions = JSON.stringify(potions)
+    last_potion_tips = JSON.stringify(power_tips_subset)
 
     clearItemsByClass('potion-marker')
 
@@ -144,7 +159,7 @@ function setPotions(potions, power_tips, character) {
 
     var items = document.getElementById('items')
     for (let i = 0; i < potions.items.length; i++) {
-        console.log('adding potion ' + i)
+        // console.log('adding potion ' + i)
 
         const power_tip_indexes = potions.items[i];
         
@@ -168,34 +183,43 @@ function setPotions(potions, power_tips, character) {
 
 function setPlayerPowers(player_powers, power_tips, character) {
 
-    console.log('set player powers, player_powers: ' + JSON.stringify(player_powers))
+    // console.log('set player powers, player_powers: ' + JSON.stringify(player_powers))
+    power_tips_subset = arraySubset(power_tips, player_powers.power_tips);
 
-    if (JSON.stringify(player_powers) == last_player_powers)
+    if (JSON.stringify(player_powers) == last_player_powers && JSON.stringify(power_tips_subset) == last_player_power_tips)
         return
 
     if (current_tooltip_id && current_tooltip_id.startsWith('player_powers'))
         current_tooltip_id = undefined
 
     last_player_powers = JSON.stringify(player_powers)
+    last_player_power_tips = JSON.stringify(power_tips_subset)
 
     clearItemsByClass('player-powers-marker')
 
     if (player_powers)
-        setCharacterPowers(player_powers, power_tips, 'player-powers-marker', 'player_powers', character)
+        setMulticolPowertips(player_powers, power_tips, 'player-powers-marker', 'player_powers', character, true)
 }
 
 
 function setMonsterPowers(monster_powers_list, power_tips, character) {
 
-    console.log('set monster powers, monster_powers_list: ' + JSON.stringify(monster_powers_list))
+    // console.log('set monster powers, monster_powers_list: ' + JSON.stringify(monster_powers_list))
 
-    if (JSON.stringify(monster_powers_list) == last_monster_powers_list)
+    var ids = []
+    for (const monster of monster_powers_list) {
+        ids = ids.concat(monster.power_tips)
+    }
+    power_tips_subset = arraySubset(power_tips, ids);
+
+    if (JSON.stringify(monster_powers_list) == last_monster_powers_list && JSON.stringify(power_tips_subset) == last_monster_powers_list_tips)
         return
 
     if (current_tooltip_id && current_tooltip_id.startsWith('monster_powers'))
         current_tooltip_id = undefined
 
     last_monster_powers_list = JSON.stringify(monster_powers_list)
+    last_monster_powers_list_tips = JSON.stringify(power_tips_subset)
 
     clearItemsByClass('monster-powers-marker')
 
@@ -203,20 +227,50 @@ function setMonsterPowers(monster_powers_list, power_tips, character) {
         for (let i = 0; i < monster_powers_list.length; i++) {
             const monster_powers = monster_powers_list[i];
 
-            setCharacterPowers(monster_powers, power_tips, 'monster-powers-marker', 'monster_powers_' + i, character)
+            setMulticolPowertips(monster_powers, power_tips, 'monster-powers-marker', 'monster_powers_' + i, character, true)
         }
 }
 
 
-function setCharacterPowers(char, power_tips, marker, id, character) {
+function setCustomTips(custom_tips_list, power_tips, character) {
+
+    // console.log('set custom tips, custom_tips_list: ' + JSON.stringify(custom_tips_list))
+
+    var ids = []
+    for (const tip of custom_tips_list) {
+        ids = ids.concat(tip.power_tips)
+    }
+    power_tips_subset = arraySubset(power_tips, ids);
+
+    if (JSON.stringify(custom_tips_list) == last_custom_tips_list && JSON.stringify(power_tips_subset) == last_custom_tips_list_tips)
+        return
+
+    if (current_tooltip_id && current_tooltip_id.startsWith('custom_tips'))
+        current_tooltip_id = undefined
+
+    last_custom_tips_list = JSON.stringify(custom_tips_list)
+    last_custom_tips_list_tips = JSON.stringify(power_tips_subset)
+
+    clearItemsByClass('custom-tips-marker')
+
+    if (custom_tips_list)
+        for (let i = 0; i < custom_tips_list.length; i++) {
+            const custom_tips = custom_tips_list[i];
+
+            setMulticolPowertips(custom_tips, power_tips, 'custom-tips-marker', 'custom_tips_' + i, character)
+        }
+}
+
+
+function setMulticolPowertips(obj, power_tips, marker, id, character, heuristic_y) {
 
     var items = document.getElementById('items')
 
-    var hitbox = char.hitbox
-    items.appendChild(createCharacterHitbox(id, hitbox.x, hitbox.y, hitbox.w, hitbox.h))
-    items.appendChild(createPowertipMulticol(id, hitbox.x, hitbox.y, hitbox.w, hitbox.h, arraySubset(power_tips, char.power_tips), character, marker))
+    var hitbox = obj.hitbox
+    items.appendChild(createHitbox(id, hitbox.x, hitbox.y, hitbox.w, hitbox.h))
+    items.appendChild(createPowertipMulticol(id, hitbox.x, hitbox.y, hitbox.w, hitbox.h, arraySubset(power_tips, obj.power_tips), character, marker))
 
-    function createCharacterHitbox(id, x, y, w, h) {
+    function createHitbox(id, x, y, w, h) {
         var hitbox = document.createElement('div')
         hitbox.className = 'powers-hitbox ' + marker
         hitbox.style = 'left: ' + x + '%; top: ' + y + '%; width: ' + w + '%; height: ' + h + '%;'
@@ -234,7 +288,7 @@ function setCharacterPowers(char, power_tips, marker, id, character) {
         var stream_width = $('#items').width()
         // var stream_height = $('#items').height()
         var stream_height = document.body.offsetHeight
-        console.log('stream height = ' + stream_height)
+        // console.log('stream height = ' + stream_height)
 
         var column = document.createElement('div')
         column.className = 'powertip-multicol-column'
@@ -246,6 +300,7 @@ function setCharacterPowers(char, power_tips, marker, id, character) {
         // create powertips and distribute them to columns
         var height = 0.0
         var max_height = 0.0
+        var power_tip_heights = []
         for (let i = 0; i < power_tips.length; i++) {
             const powertip = power_tips[i];
             
@@ -255,7 +310,9 @@ function setCharacterPowers(char, power_tips, marker, id, character) {
             height_element = powertip_elem.offsetHeight / stream_height * 100 + POWERTIP_BOTTOM_MARGIN
             temp.removeChild(powertip_elem)
 
-            console.log('element height: ' + height_element)
+            power_tip_heights.push(height_element)
+
+            // console.log('element height: ' + height_element)
             
             if (height + height_element > MAX_POWERTIP_MULTICOL_HEIGHT) {
                 if (height > max_height)
@@ -280,11 +337,11 @@ function setCharacterPowers(char, power_tips, marker, id, character) {
         // place whole multicol block
         var mc_h = max_height
         var mc_w = ncolumns * (POWERTIP_WIDTH + MULTICOL_COLUMN_RIGHT_MARGIN)
-        var mc_right_x = x + w + CHARACTER_POWERS_OFFSET
-        var mc_left_x = x - CHARACTER_POWERS_OFFSET * 3 - mc_w
-        var mc_x = 0.0
+        var mc_right_x = x + w + CHARACTER_POWERS_OFFSET_R
+        var mc_left_x = x + CHARACTER_POWERS_OFFSET_L - mc_w
+        var mc_x = 0
 
-        console.log('mc_h ' + mc_h)
+        // console.log('mc_h ' + mc_h)
         
         // decide whether the multicol block will be placed on the left or on the right
         if (mc_right_x + mc_w > MAX_RIGHT) { // would be outside of the stream on the right, display on the left
@@ -297,11 +354,22 @@ function setCharacterPowers(char, power_tips, marker, id, character) {
                 mc_x = (outside_left > outside_right) ? mc_right_x : mc_left_x
             }
         } else {
-            mc_x = mc_right_x
+            if (x + w < 80.416)
+                mc_x = mc_right_x
+            else
+                mc_x = mc_left_x
         }
 
-        var mc_y = y + h / 2 - mc_h / 2
-        console.log('mc_y ' + mc_y)
+        // console.log('power_tip_heights[0] ' + power_tip_heights[0])
+
+        if (heuristic_y) {
+            // don't worry about this line, it just approximates what is in the game. You have spent several hours on this one line. It's a heuristic, it's good enough
+            var mc_y = y + (h - CHARACTER_HEALTHBAR_HEIGHT) / 2 - (y + (h - CHARACTER_HEALTHBAR_HEIGHT)) / 100 * max_height * 0.98 + power_tip_heights[0] / 2.65
+        } else {
+            var mc_y = y + h / 2 - mc_h / 2
+        }
+            
+        // console.log('mc_y ' + mc_y)
 
         if (mc_y + mc_h > MAX_BOTTOM) { // would be below stream
             mc_y = MAX_BOTTOM - mc_h
